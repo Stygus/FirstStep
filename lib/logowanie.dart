c34932a3ed6c8478459c0ff73c70ab9c50429178
kubat <kuba.tokarczyk@onet.eu>
2025-03-21 02:28:38 +0100

    Poprawki w AppBar

--- lib/logowanie.dart
+++ lib/logowanie.dart
@@ -18,6 +18,7 @@ class Logowanie extends StatelessWidget {
                 fontSize: 32,
                 fontWeight: FontWeight.values[3],
                 color: Colors.white,
+                height: 1,
               ),
             ),
           ],
@@ -25,120 +26,126 @@ class Logowanie extends StatelessWidget {
         backgroundColor: Color(0x101010),
       ),
       backgroundColor: Color(0x101010),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: 10),
-              Image.asset(
-                'assets/images/linia.png', // Ścieżka do obrazu
-                height: 100,
-                width: 1000, // Wysokość obrazu
-                fit: BoxFit.cover,
-              ),
-              // Tytuł aplikacji
-              Text(
-                'FirstStep',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 20),
-              // Pole do wpisania adresu e-mail
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Adres e-mail',
-                  labelStyle: TextStyle(color: Colors.black),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                ),
-                style: TextStyle(color: Colors.black),
-              ),
-              SizedBox(height: 16),
-              // Pole do wpisania hasła
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Hasło',
-                  labelStyle: TextStyle(color: Colors.black),
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                ),
-                style: TextStyle(color: Colors.black),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              // Przycisk logowania
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    // Akcja po naciśnięciu przycisku
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Przycisk logowania kliknięty!')),
-                    );
-
-                    // przejście do menu głównego
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      // Poziomy prostokąt plusa
-                      Container(
-                        width: 110,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      // Pionowy prostokąt plusa
-                      Container(
-                        width: 50,
-                        height: 110,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      // Tekst na przycisku
-                      Text(
-                        'Zaloguj',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Link do rejestracji
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/rejestracja');
-                  },
-                  child: Text(
-                    'Nie masz konta? \n Zarejestruj się',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
+      body: Column(
+        children: [
+          Image.asset(
+            'assets/images/linia.png', // Ścieżka do obrazu
+            height: 50, // Wysokość obrazu
+            width: 1000, // Wysokość obrazu
+            fit: BoxFit.cover,
           ),
-        ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 10),
+
+                  // Tytuł aplikacji
+                  Text(
+                    'FirstStep',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Pole do wpisania adresu e-mail
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Adres e-mail',
+                      labelStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(height: 16),
+                  // Pole do wpisania hasła
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Hasło',
+                      labelStyle: TextStyle(color: Colors.black),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.black),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20),
+                  // Przycisk logowania
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        // Akcja po naciśnięciu przycisku
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text('Przycisk logowania kliknięty!')),
+                        );
+
+                        // przejście do menu głównego
+                      },
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          // Poziomy prostokąt plusa
+                          Container(
+                            width: 110,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          // Pionowy prostokąt plusa
+                          Container(
+                            width: 50,
+                            height: 110,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          // Tekst na przycisku
+                          Text(
+                            'Zaloguj',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  // Link do rejestracji
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/rejestracja');
+                      },
+                      child: Text(
+                        'Nie masz konta? \n Zarejestruj się',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
