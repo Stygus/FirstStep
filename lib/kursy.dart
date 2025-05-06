c6508e13f24bef3e12da4a8e8ea10c5a663f25d5
Tokarczyk_Jakub <kuba.tokarczyk@onet.eu>
2025-05-06 11:53:04 +0200

    Ustawienie naglowka kursow

--- lib/kursy.dart
+++ lib/kursy.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class KursyPage extends StatelessWidget {
   const KursyPage({super.key});
@@ -6,25 +7,79 @@ class KursyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        toolbarHeight: 0,
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       backgroundColor: Color(0xFF101010),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              'Kursy\n Już wkrótce!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Kursy',
+                      style: GoogleFonts.itim(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.width * (3 / 16),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        alignment: Alignment.bottomCenter,
+                        image: AssetImage('assets/images/linia.png'),
+                        fit: BoxFit.fill,
+                        isAntiAlias: false,
+                        filterQuality: FilterQuality.high,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    'Kursy\n Już wkrótce!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Image.asset(
+                    'assets/images/logod.png',
+                    height: 200,
+                    width: 200,
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 60),
-            Image.asset('assets/images/logod.png', height: 200, width: 200),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
