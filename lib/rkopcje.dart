149c283a5b1cfcc2e4116db6ffda94ccaa5d6cf4
Tokarczyk_Jakub <kuba.tokarczyk@onet.eu>
2025-05-20 14:06:09 +0200

    Wstepne ukonczenie menu rko

--- lib/rkopcje.dart
+++ lib/rkopcje.dart
@@ -1,5 +1,6 @@
 import 'package:firststep/rkosymulator_dorosli.dart';
 import 'package:firststep/rkosymulator_dziecko.dart';
+import 'package:firststep/rkosymulator_niemowle.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -113,7 +114,14 @@ class RkOpcje extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RkoSymulatorNiemowle(),
+                        ),
+                      );
+                    },
                     child: ConstrainedBox(
                       constraints: BoxConstraints(maxHeight: imageHeight),
                       child: Image.asset(
